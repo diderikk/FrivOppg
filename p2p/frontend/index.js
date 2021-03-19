7e28068c0e7cb790e77d76c902756c104e10c783
Diderik Kramer <diderikk@stud.ntnu.no>
2021-03-19 22:07:08 +0100

    Day 5

--- p2p/frontend/index.js
+++ p2p/frontend/index.js
@@ -4,19 +4,22 @@ let chatChannel = null;
 let sessionID = '';
 
 
+
 const configuration = { iceServers: [{ urls: 'stun:stun1.l.google.com:19302' }] }
 const WebRTCConnection = new RTCPeerConnection(configuration);
 
 window.onload = () => {
+    playVideoFromCamera();
+    playVideoFromRemoteCamera()
 
     document.getElementById('create').onclick = () => {
         ws.onmessage = event => {
             // console.log(event.data);
-            if ((/^\d+/).test(event.data)){
+            if ((/^\d+/).test(event.data)) {
                 document.getElementById('created-session-id').innerText = "Session ID: " + event.data;
                 document.getElementById('created-session-id').style.visibility = 'visible';
-                // document.getElementById('session-id-input').setAttribute('display', 'none');
-                // document.getElementById('connect').setAttribute('display','none');
+                document.getElementById('session-id-input').setAttribute('disabled', 'disabled');
+                document.getElementById('connect').setAttribute('disabled', 'disabled');
                 return;
             }
             const remoteDescription = JSON.parse(event.data);
@@ -29,36 +32,66 @@ window.onload = () => {
         event.preventDefault();
         ws.onmessage = event => {
             // console.log(event.data);
-            if(event.data === 'This is not a valid Session ID'){
+            if (event.data === 'This is not a valid Session ID') {
                 document.getElementById('error').innerText = event.data;
                 return;
             };
+            document.getElementById('create').setAttribute('disabled','disabled');
             joinRoom(JSON.parse(event.data));
         }
         sessionID = document.getElementById('session-id-input').value;
-        send(sessionID, null,false,false);
+        send(sessionID, null, false, false);
     }
 
-    document.getElementById("submit").addEventListener("click",(event) => {
+    document.getElementById("send").addEventListener("click",(event) => {
         event.preventDefault();
-        let input = document.getElementById('message').value;
-        document.getElementById('chat').value += 'You: ' + input + '\n';
+        let input = document.getElementById('message-input').value;
+        document.getElementById('output').value += '\n' + 'You: ' + input;
         chatChannel.send(input);
-        document.getElementById('message').value = '';
+        document.getElementById('message-input').value = '';
       });
+
 
 }
 
+async function playVideoFromCamera() {
+    try {
+        const constraints = { video: true, audio: true };
+        const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+        const video = document.getElementById('local-video');
 
+        localStream.getTracks().forEach((track) => {
+            WebRTCConnection.addTrack(track, localStream);
+        });
+
+        video.srcObject = localStream;
+        video.muted = true;
+
+    } catch (error) {
+        console.error("Could not open video", error);
+    }
+}
+
+async function playVideoFromRemoteCamera() {
+    let remoteStream = new MediaStream();
+    const remoteVideo = document.getElementById('remote-video');
+    remoteVideo.srcObject = remoteStream;
+
+    WebRTCConnection.addEventListener('track', async (event) => {
+        remoteStream.addTrack(event.track, remoteStream);
+    })
+}
 
 function createRoom() {
     chatChannel = WebRTCConnection.createDataChannel('chat');
     chatChannel.onmessage = (event) => {
-        document.getElementById('chat').value += 'Other dude: ' + event.data + '\n';
+        document.getElementById('output').value += '\n' +'Other dude: ' + 
+        event.data;
+        console.log(event.data);
     }
     chatChannel.onopen = () => {
         console.log("Open");
-        document.getElementById('submit').removeAttribute('disabled');
+        document.getElementById('send').removeAttribute('disabled');
         ws.close();
     }
     chatChannel.onclose = () => console.log('onclose');
@@ -84,11 +117,12 @@ function joinRoom(remoteDescription) {
         if (event.channel.label == 'chat') {
             chatChannel = event.channel;
             chatChannel.onmessage = (event) => {
-                document.getElementById('chat').value += 'Other dude: ' + event.data + '\n';
+                document.getElementById('output').value += '\n' + 'Other dude: ' + event.data
+                console.log(event.data);
             }
             chatChannel.onopen = () => {
                 console.log("Open");
-                document.getElementById('submit').removeAttribute('disabled');
+                document.getElementById('send').removeAttribute('disabled');
                 ws.close();
             }
             chatChannel.onclose = () => console.log('onclose');
@@ -96,13 +130,12 @@ function joinRoom(remoteDescription) {
     };
 
     WebRTCConnection.onicecandidate = (event) => {
-
-        if(WebRTCConnection.localDescription.toJSON().type === 'answer' && !localDescription){
+        if (WebRTCConnection.localDescription.toJSON().type === 'answer' && !localDescription) {
             // console.log(JSON.stringify(WebRTCConnection.localDescription));
             localDescription = WebRTCConnection.localDescription;
-            send(sessionID,localDescription, true, false);
+            send(sessionID, localDescription, true, false);
         }
-    };    
+    };
 
     WebRTCConnection.setRemoteDescription(remoteDescription);
 
