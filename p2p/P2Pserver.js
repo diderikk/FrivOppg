0e8d6d62c06c05a2839e7427cd2c0299e29b9c67
Diderik Kramer <diderikk@stud.ntnu.no>
2021-03-23 11:21:36 +0100

    Test2

--- p2p/P2Pserver.js
+++ p2p/P2Pserver.js
@@ -53,7 +53,7 @@ const httpServer = http.createServer((req, res) => {
   }
 });
 
-// Listens on port 3000
+// Listen on port 3000
 httpServer.listen(HTTPPORT, () => {
   console.log("Server listening on port", HTTPPORT);
 });
